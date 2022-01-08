1abc35857f9ed9b6792c3a633d069fbc264471dd
Nate Sales <nate@natesales.net>
2022-01-07 22:45:58 -0800

    feat: add open buttons

--- script.js
+++ script.js
@@ -10,6 +10,21 @@ window.onload = function () {
     // Bind image load handler
     let img = document.getElementById("form-logo");
     img.addEventListener("change", load_image, false);
+
+    // document.getElementById("form-prefixes").value = "192.0.2.0/24\n2001:db8::/48";
+    // document.getElementById("form-network-name").value = "Example Network";
+    // document.getElementById("form-asn").value = 65530;
+    // document.getElementById("form-peer-name").value = "Example Peer";
+    // document.getElementById("form-peer-asn").value = 65510;
+    // document.getElementById("form-contact-name").value = "Example User";
+    // document.getElementById("form-email").value = "noc@example.com";
+    // document.getElementById("form-phone").value = "000 000 0000";
+    // document.getElementById("form-notes").value = "This\nis\na\ntest";
+}
+
+function date_string() {
+    let today = new Date();
+    return `${today.getDate()}-${today.getMonth() + 1}-${today.getFullYear()}`
 }
 
 function form_ok() {
@@ -18,6 +33,9 @@ function form_ok() {
         if (!all_filled) return;
         if (!i.value || i.value === "") all_filled = false;
     })
+    if (!all_filled) {
+        alert("Please complete all required fields")
+    }
     return all_filled;
 }
 
@@ -63,12 +81,13 @@ function generate_loa() {
 
 To whom it may concern,
 
-This letter serves to authorize AS` + peer_asn + ` (` + peer_name + `) to announce the following IP address blocks:
+This letter serves to authorize AS${peer_asn} (${peer_name}) to announce the following IP address blocks:
 
-` + prefixes + `
+${prefixes}
 
-As a representative of AS` + asn + ` (` + name + `) that is the owner of the prefix(es) and/or ASN, I hereby affirm that I'm authorized to represent and sign for this LOA.
+As a representative of AS${asn} (${name}) that is the owner of the prefix(es) and/or ASN, I hereby affirm that I'm authorized to represent and sign for this LOA.
 `
+
     if (notes !== "") {
         loa_body += "\n" + notes + "\n"
     }
@@ -88,30 +107,29 @@ Sincerely,
     return loa_body
 }
 
-function save_text_loa() {
+function gen_text_loa(download) {
     if (!form_ok()) {
         return
     }
 
     let peer_asn = document.getElementById("form-peer-asn").value.toUpperCase().replace("AS", "");
     let loa_body = "Letter of Authorization\n\n" + generate_loa();
+    let target_text = "data:text/plain;charset=utf-8," + encodeURIComponent(loa_body);
 
-    let element = document.createElement("a");
-    element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(loa_body));
-    element.setAttribute("download", "LoA_AS" + peer_asn + "_" + moment().format("MM-DD-YYYY") + ".txt");
-
-    element.style.display = "none";
-    document.body.appendChild(element);
-
-    element.click();
-
-    document.body.removeChild(element);
+    if (download) {
+        let element = document.createElement("a");
+        element.style.display = "none";
+        element.setAttribute("href", target_text);
+        element.setAttribute("download", "LoA_AS" + peer_asn + "_" + date_string() + ".txt");
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
+    } else {
+        alert(loa_body)
+    }
 }
 
-function save_pdf_loa() {
-    if (!form_ok()) {
-        return
-    }
+function loa_pdf_doc() {
     let doc = new jspdf.jsPDF();
 
     let header_height = 20;
@@ -124,7 +142,7 @@ function save_pdf_loa() {
         body_height = 60;
 
         let target_height = 20;
-        let target_width = (logo.clientWidth/logo.clientHeight)*target_height;
+        let target_width = (logo.clientWidth / logo.clientHeight) * target_height;
         doc.addImage(logo.src, 20, 20, target_width, target_height, "NONE", 0);
     }
 
@@ -134,12 +152,31 @@ function save_pdf_loa() {
     doc.setFontSize(12);
     let loa_body = generate_loa();
     doc.text(20, body_height, doc.splitTextToSize(loa_body, 180));
+    return doc
+}
+
+function save_pdf_loa() {
+    if (!form_ok()) {
+        return
+    }
+
+    let doc = loa_pdf_doc();
     let peer_asn = document.getElementById("form-peer-asn").value.toUpperCase().replace("AS", "");
-    doc.save("LoA_AS" + peer_asn + "_" + moment().format("MM-DD-YYYY") + ".pdf");
+    doc.save("LoA_AS" + peer_asn + "_" + date_string() + ".pdf");
+}
+
+function open_pdf_loa() {
+    if (!form_ok()) {
+        return
+    }
+
+    let doc = loa_pdf_doc();
+    doc.output('dataurlnewwindow');
 }
 
 function load_image(e) {
     let logo_preview = document.getElementById("logo-preview");
+    logo_preview.style.display = "block";
 
     let reader = new FileReader();
     reader.onload = function (event) {
